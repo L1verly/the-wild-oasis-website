a5e09cb1a01b5efeef3d60d070119f40b1c1768b
L1verly <artemartem7975@gmail.com>
2026-03-22 16:24:35 +0100

    Adding filtering for cabins

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -6,9 +6,18 @@ export default async function CabinList() {
 
   if (cabins.length < 0) return null;
 
+  let displayedCabins;
+  if (filter === "all") displayedCabins = cabins;
+  else if (filter === "small")
+    displayedCabins = cabins.filter((cabin) => cabin.maxcapacity <= 3);
+  else if (filter === "medium")
+    displayedCabins = cabins.filter((cabin) => cabin.maxcapacity <= 7);
+  else if (filter === "large")
+    displayedCabins = cabins.filter((cabin) => cabin.maxcapacity >= 8);
+
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {displayedCabins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
