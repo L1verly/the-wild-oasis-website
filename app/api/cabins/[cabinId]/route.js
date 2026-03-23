e418fdb5e0911d0b60feeb541e1df62526cf6f46
L1verly <artemartem7975@gmail.com>
2026-03-23 21:37:00 +0100

    setting up api route handler for fetching cabin info and booked dates

---
+++ app/api/cabins/[cabinId]/route.js
@@ -0,0 +1,18 @@
+import { getBookedDatesByCabinId, getCabin } from "@/_lib/data-service";
+import { createSupabaseServerClient } from "@/_lib/supabase";
+
+export async function GET(request, { params }) {
+  const { cabinId } = params;
+
+  try {
+    const supabase = await createSupabaseServerClient();
+    const [cabin, bookedDates] = await Promise.all([
+      getCabin(supabase, cabinId),
+      getBookedDatesByCabinId(supabase, cabinId),
+    ]);
+
+    return Response.json({ cabin, bookedDates });
+  } catch (error) {
+    return Response.json({ message: "Cabin not found" });
+  }
+}
