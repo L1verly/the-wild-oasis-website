58736a6544b3cd484f8ba8bb209069ed7587af7f
L1verly <artemartem7975@gmail.com>
2026-03-23 17:03:29 +0100

    CabinList typo fix

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -1,19 +1,22 @@
 import { getCabins } from "@/_lib/data-service";
 import CabinCard from "./CabinCard";
+import { createSupabaseServerClient } from "@/_lib/supabase";
 
-export default async function CabinList() {
-  const cabins = await getCabins();
+export default async function CabinList({ filter }) {
+  const supabase = await createSupabaseServerClient();
+
+  const cabins = await getCabins(supabase);
 
   if (cabins.length < 0) return null;
 
   let displayedCabins;
   if (filter === "all") displayedCabins = cabins;
   else if (filter === "small")
-    displayedCabins = cabins.filter((cabin) => cabin.maxcapacity <= 3);
+    displayedCabins = cabins.filter((cabin) => cabin.maxCapacity <= 3);
   else if (filter === "medium")
-    displayedCabins = cabins.filter((cabin) => cabin.maxcapacity <= 7);
+    displayedCabins = cabins.filter((cabin) => cabin.maxCapacity <= 7);
   else if (filter === "large")
-    displayedCabins = cabins.filter((cabin) => cabin.maxcapacity >= 8);
+    displayedCabins = cabins.filter((cabin) => cabin.maxCapacity >= 8);
 
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
