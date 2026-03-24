8fbf3fac93366bc8a15117dc6640ce4a58953406
L1verly <artemartem7975@gmail.com>
2026-03-24 11:24:16 +0100

    adding middleware for auth protecting /account

---
+++ middleware.js
@@ -0,0 +1,21 @@
+import { betterFetch } from "@better-fetch/fetch";
+import { NextResponse } from "next/server";
+
+export async function middleware(request) {
+  const { data: session } = await betterFetch("/api/auth/get-session", {
+    baseURL: request.nextUrl.origin,
+    headers: {
+      cookie: request.headers.get("cookie") || "", // Forward the cookies from the request
+    },
+  });
+
+  if (!session) {
+    return NextResponse.redirect(new URL("/about", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/account"], // Apply middleware to specific routes
+};
