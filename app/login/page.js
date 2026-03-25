1f23317d66a273886d770db4ca7657b5c378e4f5
L1verly <artemartem7975@gmail.com>
2026-03-25 12:47:00 +0100

    Protectiong login route against already logged in user

--- app/login/page.js
+++ app/login/page.js
@@ -1,10 +1,19 @@
 import SignInButton from "@/_components/SignInButton";
+import { auth } from "@/_lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 export const metadata = {
   title: "Login",
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (session) {
+    redirect("/account");
+  }
   return (
     <div className="flex flex-col gap-10 mt-10 items-center">
       <h2 className="text-3xl font-semibold">
