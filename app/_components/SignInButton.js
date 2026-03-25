c27ea9f9e26868a3ed98fc9af4be53b74fe2b0ca
L1verly <artemartem7975@gmail.com>
2026-03-25 10:39:51 +0100

    signIn signOut buttons styling fix

--- app/_components/SignInButton.js
+++ app/_components/SignInButton.js
@@ -6,7 +6,7 @@ import Image from "next/image";
 function SignInButton() {
   return (
     <button
-      className="flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium hover:bg-primary-800 focus:bg-primary-800 cursor-pointer"
+      className="flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium hover:bg-primary-800 hover:text-primary-100 transition-colors focus:bg-primary-800 cursor-pointer"
       onClick={signIn}
     >
       <Image src="/google-logo.svg" alt="Google logo" height="24" width="24" />
