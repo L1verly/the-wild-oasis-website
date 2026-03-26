8cfbbad4875c062a6012c877f32940491a032efb
L1verly <artemartem7975@gmail.com>
2026-03-26 20:58:43 +0100

    Optimistically deleting reservation

--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -1,9 +1,9 @@
 import Link from "next/link";
-import ReservationCard from "@/_components/ReservationCard";
 import { createSupabaseServerClient } from "@/_lib/supabase/supabase-server";
 import { getBookings } from "@/_lib/data-service";
 import { auth } from "@/_lib/auth";
 import { headers } from "next/headers";
+import ReservationList from "@/_components/ReservationList";
 
 export const metadata = {
   title: "Reservations",
@@ -29,12 +29,7 @@ export default async function Page() {
             luxury cabins &rarr;
           </Link>
         </p>
-      : <ul className="space-y-6">
-          {bookings.map((booking) => (
-            <ReservationCard booking={booking} key={booking.id} />
-          ))}
-        </ul>
-      }
+      : <ReservationList bookings={bookings} />}
     </div>
   );
 }
